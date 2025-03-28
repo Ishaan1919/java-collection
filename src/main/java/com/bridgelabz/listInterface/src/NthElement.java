c42d8b2caea3a797d07d381a414eeec1116ee5e4
Ishaan <ishaan0642.be21@chitkara.edu.in>
2025-03-28 13:24:29 +0530

    Program for find the nth element from the last in a singly linked list

---
+++ src/main/java/com/bridgelabz/listInterface/src/NthElement.java
@@ -0,0 +1,28 @@
+package com.bridgelabz.listInterface.src;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class NthElement {
+    public static void main(String[] args) {
+        LinkedList<Character> list = new LinkedList<>();
+        int nthIndex = 2;
+//    [A, B, C, D, E]
+        list.add('A');
+        list.add('B');
+        list.add('C');
+        list.add('D');
+        list.add('E');
+        int ptr = 0;
+        int ans = 0;
+        int size = list.size();
+        for(int i=0;i<nthIndex;i++){
+            ptr++;
+        }
+        while(ptr<size){
+            ptr++;
+            ans++;
+        }
+        System.out.println(list.get(ans));
+    }
+}
